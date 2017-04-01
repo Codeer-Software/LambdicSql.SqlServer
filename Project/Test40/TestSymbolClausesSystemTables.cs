9456ffc5bbfe1c87c04943a0463f7f2c6dffd44d
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-01 09:33:29 +0900

    add tests.

---
+++ Project/Test40/TestSymbolClausesSystemTables.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LambdicSql;
+using LambdicSql.feat.Dapper;
+using static LambdicSql.SqlServer.Symbol;
+using Test.Helper;
+using LambdicSql.SqlServer;
+
+namespace Test
+{
+    [TestClass]
+    public class TestSymbolClausesSystemTables
+    {
+        public TestContext TestContext { get; set; }
+        public IDbConnection _connection;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _connection = TestEnvironment.CreateConnection(TestContext);
+            _connection.Open();
+        }
+
+        [TestCleanup]
+        public void TestCleanup() => _connection.Dispose();
+        
+    }
+}
