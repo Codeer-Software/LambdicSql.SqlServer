64f7134794a5bb7bb4c9605497459770a11fc9ac
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-01 12:26:53 +0900

    implements mathematical functions.

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Mathematical.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Mathematical.cs
@@ -1,52 +1,286 @@
 using LambdicSql.ConverterServices;
 using LambdicSql.ConverterServices.SymbolConverters;
-using System;
 
 namespace LambdicSql.SqlServer
 {
     public static partial class Symbol
     {
-        /*
-        ACOS
-        ASIN
-        ATAN
-        ATN2
-        CEILING
-        COS
-        COT
-        DEGREES
-        EXP
-        FLOOR
-        LOG
-        LOG10
-        PI
-        POWER
-        RADIANS
-        RAND
-        SIGN
-        SIN
-        SQRT
-        SQUARE
-        TAN
-         */
         /// <summary>
         /// ABS function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/abs-transact-sql
         /// </summary>
-        /// <typeparam name="T">Type represented by expression.</typeparam>
-        /// <param name="column">The column or expression that is function target.</param>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
         /// <returns>Absolute value.</returns>
         [FuncStyleConverter]
-        public static T Abs<T>(T column) { throw new InvalitContextException(nameof(Abs)); }
+        public static decimal Abs(decimal numeric_expression) { throw new InvalitContextException(nameof(Abs)); }
+
+        /// <summary>
+        /// ACOS function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/acos-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>ACOS value.</returns>
+        [FuncStyleConverter]
+        public static double Acos(double float_expression) { throw new InvalitContextException(nameof(Acos)); }
+
+        /// <summary>
+        /// ASIN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/asin-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>summary value.</returns>
+        [FuncStyleConverter]
+        public static double Asin(double float_expression) { throw new InvalitContextException(nameof(Asin)); }
+
+        /// <summary>
+        /// ATAN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/atan-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>ATAN value.</returns>
+        [FuncStyleConverter]
+        public static double Atan(double float_expression) { throw new InvalitContextException(nameof(Atan)); }
+
+        /// <summary>
+        /// ATN2 function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/atn2-transact-sql
+        /// </summary>
+        /// <param name="float_expression1">The column or expression that is function target.</param>
+        /// <param name="float_expression2">The column or expression that is function target.</param>
+        /// <returns>ATN2 value.</returns>
+        [FuncStyleConverter]
+        public static double Atn2(double float_expression1, double float_expression2) { throw new InvalitContextException(nameof(Atn2)); }
+
+        /// <summary>
+        /// CEILING function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/ceiling-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>CEILING value.</returns>
+        [FuncStyleConverter]
+        public static decimal Ceiling(decimal numeric_expression) { throw new InvalitContextException(nameof(Ceiling)); }
+
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/cos-transact-sql
+        /// COS function.
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>COS value.</returns>
+        [FuncStyleConverter]
+        public static double Cos(double float_expression) { throw new InvalitContextException(nameof(Cos)); }
+
+        /// <summary>
+        /// COT function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/cot-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>COT value.</returns>
+        [FuncStyleConverter]
+        public static double Cot(double float_expression) { throw new InvalitContextException(nameof(Cot)); }
+
+        /// <summary>
+        /// DEGREES function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/degrees-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>DEGREES value.</returns>
+        [FuncStyleConverter]
+        public static decimal Degrees(decimal numeric_expression) { throw new InvalitContextException(nameof(Degrees)); }
+
+        /// <summary>
+        /// EXP function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/exp-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>EXP value.</returns>
+        [FuncStyleConverter]
+        public static double Exp(double float_expression) { throw new InvalitContextException(nameof(Ceiling)); }
+
+        /// <summary>
+        /// FLOOR function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/floor-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>FLOOR value.</returns>
+        [FuncStyleConverter]
+        public static decimal Floor(decimal numeric_expression) { throw new InvalitContextException(nameof(Floor)); }
+
+        /// <summary>
+        /// LOG function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/log-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>LOG value.</returns>
+        [FuncStyleConverter]
+        public static double Log(double float_expression) { throw new InvalitContextException(nameof(Log)); }
+
+        /// <summary>
+        /// LOG10 function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/log10-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>LOG10 value.</returns>
+        [FuncStyleConverter]
+        public static double Log10(double float_expression) { throw new InvalitContextException(nameof(Log10)); }
+
+        /// <summary>
+        /// PI function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/pi-transact-sql
+        /// </summary>
+        /// <returns>PI value.</returns>
+        [FuncStyleConverter]
+        public static double PI() { throw new InvalitContextException(nameof(Log)); }
+
+        /// <summary>
+        /// POWER function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/power-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <param name="y">The column or expression that is function target.</param>
+        /// <returns>POWER value.</returns>
+        [FuncStyleConverter]
+        public static double Power(double float_expression, int y) { throw new InvalitContextException(nameof(Log)); }
+
+
+        /// <
+        /// <summary>
+        /// RADIANS function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/radians-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>RADIANS value.</returns>
+        [FuncStyleConverter]
+        public static decimal Radians(decimal numeric_expression) { throw new InvalitContextException(nameof(Sign)); }
+        
+        /// <summary>
+        /// RAND function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/rand-transact-sql
+        /// </summary>
+        /// <param name="seed">seed value.</param>
+        /// <returns>RAND value.</returns>
+        [FuncStyleConverter]
+        public static double Rand(int seed) { throw new InvalitContextException(nameof(Sign)); }
 
         /// <summary>
         /// ROUND function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/round-transact-sql
         /// </summary>
-        /// <typeparam name="T1">Type represented by target.</typeparam>
-        /// <typeparam name="T2">Type represented by digit</typeparam>
         /// <param name="target">Numeric expression to round.</param>
         /// <param name="digit">Is the precision to which it is to be rounded.</param>
         /// <returns>Rounded result.</returns>
         [FuncStyleConverter]
         public static T1 Round<T1, T2>(T1 target, T2 digit) { throw new InvalitContextException(nameof(Round)); }
+
+        /// <summary>
+        /// ROUND function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/round-transact-sql
+        /// </summary>
+        /// <param name="target">Numeric expression to round.</param>
+        /// <param name="digit">Is the precision to which it is to be rounded.</param>
+        /// <returns>Rounded result.</returns>
+        [FuncStyleConverter]
+        public static long Round(long target, int digit) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// ROUND function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/round-transact-sql
+        /// </summary>
+        /// <param name="target">Numeric expression to round.</param>
+        /// <param name="digit">Is the precision to which it is to be rounded.</param>
+        /// <returns>Rounded result.</returns>
+        [FuncStyleConverter]
+        public static int Round(int target, int digit) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// ROUND function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/round-transact-sql
+        /// </summary>
+        /// <param name="target">Numeric expression to round.</param>
+        /// <param name="digit">Is the precision to which it is to be rounded.</param>
+        /// <returns>Rounded result.</returns>
+        [FuncStyleConverter]
+        public static decimal Round(decimal target, int digit) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// ROUND function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/round-transact-sql
+        /// </summary>
+        /// <param name="target">Numeric expression to round.</param>
+        /// <param name="digit">Is the precision to which it is to be rounded.</param>
+        /// <returns>Rounded result.</returns>
+        [FuncStyleConverter]
+        public static double Round(double target, int digit) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// SIGN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sign-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>SIGN value.</returns>
+        [FuncStyleConverter]
+        public static long Sign(long numeric_expression) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// SIGN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sign-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>SIGN value.</returns>
+        [FuncStyleConverter]
+        public static int Sign(int numeric_expression) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// SIGN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sign-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>SIGN value.</returns>
+        [FuncStyleConverter]
+        public static decimal Sign(decimal numeric_expression) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// SIGN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sign-transact-sql
+        /// </summary>
+        /// <param name="numeric_expression">The column or expression that is function target.</param>
+        /// <returns>SIGN value.</returns>
+        [FuncStyleConverter]
+        public static double Sign(double numeric_expression) { throw new InvalitContextException(nameof(Sign)); }
+
+        /// <summary>
+        /// SIN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sin-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>SIN value.</returns>
+        [FuncStyleConverter]
+        public static double Sin(double float_expression) { throw new InvalitContextException(nameof(Sin)); }
+
+        /// <summary>
+        /// SQRT function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/sqrt-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>SQRT value.</returns>
+        [FuncStyleConverter]
+        public static double Sqrt(double float_expression) { throw new InvalitContextException(nameof(Log)); }
+
+        /// <summary>
+        /// SQUARE function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/square-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>SQUARE value.</returns>
+        [FuncStyleConverter]
+        public static double Square(double float_expression) { throw new InvalitContextException(nameof(Log)); }
+
+        /// <summary>
+        /// TAN function.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/tan-transact-sql
+        /// </summary>
+        /// <param name="float_expression">The column or expression that is function target.</param>
+        /// <returns>TAN value.</returns>
+        [FuncStyleConverter]
+        public static double Tan(double float_expression) { throw new InvalitContextException(nameof(Tan)); }
     }
 }
--- Project/Test40/TestSymbolFuncsMathematical.cs
+++ Project/Test40/TestSymbolFuncsMathematical.cs
@@ -26,13 +26,12 @@ namespace Test
         public void TestCleanup() => _connection.Dispose();
 
         [TestMethod]
-        public void Test_Abs_Round()
+        public void Test_Abs()
         {
             var sql = Db<DB>.Sql(db =>
                 Select(new
                 {
-                    Val1 = Abs(1),
-                    Val2 = Round(2, 3),
+                    Val1 = Abs(1)
                 }).
                 From(db.tbl_staff));
 
@@ -40,9 +39,416 @@ namespace Test
             Assert.IsTrue(0 < datas.Count);
             AssertEx.AreEqual(sql, _connection,
 @"SELECT
-	ABS(@p_0) AS Val1,
-	ROUND(@p_1, @p_2) AS Val2
-FROM tbl_staff", 1, 2, 3);
+	ABS(@p_0) AS Val1
+FROM tbl_staff", (decimal)1);
+        }
+
+        [TestMethod]
+        public void Test_Acos()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Acos(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ACOS(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Asin()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Asin(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ASIN(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Atan()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Atan(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ATAN(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Atn2()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Atn2(1, 1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ATN2(@p_0, @p_1) AS Val1
+FROM tbl_staff", (double)1, (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Ceiling()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Ceiling(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	CEILING(@p_0) AS Val1
+FROM tbl_staff", (decimal)1);
+        }
+
+        [TestMethod]
+        public void Test_Cos()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Cos(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	COS(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Cot()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Cot(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	COT(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Degrees()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Degrees(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	DEGREES(@p_0) AS Val1
+FROM tbl_staff", (decimal)1);
+        }
+
+        [TestMethod]
+        public void Test_Exp()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Exp(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	EXP(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Floor()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Floor(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	FLOOR(@p_0) AS Val1
+FROM tbl_staff", (decimal)1);
+        }
+
+        [TestMethod]
+        public void Test_Log()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Log(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	LOG(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Log10()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Log10(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	LOG10(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_PI()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = PI(),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	PI() AS Val1
+FROM tbl_staff");
+        }
+
+        [TestMethod]
+        public void Test_Power()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Power(1, 2),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	POWER(@p_0, @p_1) AS Val1
+FROM tbl_staff", (double)1, 2);
+        }
+
+        [TestMethod]
+        public void Test_Radians()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Radians(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	RADIANS(@p_0) AS Val1
+FROM tbl_staff", (decimal)1);
+        }
+
+        [TestMethod]
+        public void Test_Rand()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Rand(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	RAND(@p_0) AS Val1
+FROM tbl_staff", 1);
+        }
+
+        [TestMethod]
+        public void Test_Round()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Round((long)2, 3),
+                    Val2 = Round(2, 3),
+                    Val3 = Round((decimal)2, 3),
+                    Val4 = Round((double)2, 3)
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ROUND(@p_0, @p_1) AS Val1,
+	ROUND(@p_2, @p_3) AS Val2,
+	ROUND(@p_4, @p_5) AS Val3,
+	ROUND(@p_6, @p_7) AS Val4
+FROM tbl_staff", (long)2, 3, 2, 3, (decimal)2, 3, (double)2, 3);
+        }
+
+        [TestMethod]
+        public void Test_Sign()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Sign((long)1),
+                    Val2 = Sign(1),
+                    Val3 = Sign((decimal)1),
+                    Val4 = Sign((double)1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	SIGN(@p_0) AS Val1,
+	SIGN(@p_1) AS Val2,
+	SIGN(@p_2) AS Val3,
+	SIGN(@p_3) AS Val4
+FROM tbl_staff", (long)1, 1, (decimal)1, (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Sin()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Sin(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	SIN(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Sqrt()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Sqrt(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	SQRT(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Square()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Square(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	SQUARE(@p_0) AS Val1
+FROM tbl_staff", (double)1);
+        }
+
+        [TestMethod]
+        public void Test_Tan()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    Val1 = Tan(1),
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	TAN(@p_0) AS Val1
+FROM tbl_staff", (double)1);
         }
     }
 }
