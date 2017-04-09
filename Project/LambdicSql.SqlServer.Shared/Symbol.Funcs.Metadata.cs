b6cd781a9932f63d37f001e2b5a911279cf5caef
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-09 21:57:18 +0900

    implementing...

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Metadata.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Metadata.cs
@@ -14,6 +14,10 @@ namespace LambdicSql.SqlServer
         /// <returns>object id.</returns>
         [MethodFormatConverter(Format = "OBJECT_ID('[0]')")]
         public static int? Object_Id(object obj) { throw new InvalitContextException(nameof(Constraint)); }
+        //TODO type.
+        //やっぱり、stringかなー
+        //ToSqlString的なHelper考えるか？
+
         /*
         @@PROCID
         APP_NAME
@@ -41,7 +45,7 @@ namespace LambdicSql.SqlServer
         INDEXPROPERTY
         NEXT VALUE FOR
         OBJECT_DEFINITION
-        OBJECT_ID → special version.
+        OBJECT_ID 
         OBJECT_NAME
         OBJECT_SCHEMA_NAME
         OBJECTPROPERTY
@@ -57,5 +61,54 @@ namespace LambdicSql.SqlServer
         TYPE_NAME
         TYPEPROPERTY
         VERSION */
+
+
+
+
+        /*
+select object_name(object_id('tbl_staff'));
+select OBJECT_DEFINITION(object_id('tbl_staff'));
+select OBJECT_SCHEMA_NAME(object_id('tbl_staff'));
+
+select OBJECTPROPERTY(object_id('tbl_staff'), 'CnstIsClustKey');
+select OBJECTPROPERTYEX(object_id('tbl_staff'), 'CnstIsClustKey');
+
+select OBJECT_NAME(@@PROCID); 
+
+
+
+select APP_NAME(); 
+
+
+//SELECT APPLOCK_MODE('public', 'Form1', 'Transaction');
+//select APPLOCK_TEST ( 'database_principal' , 'resource_name' , 'lock_mode' , 'lock_owner' );
+
+select ASSEMBLYPROPERTY ('HelloWorld' , 'PublicKey');
+select COL_LENGTH('tbl_staff', 'id');  
+select COL_Name(object_id('tbl_staff'), 1);
+
+SELECT COLUMNPROPERTY(object_id('tbl_staff'),'id','PRECISION'); 
+
+SELECT DATABASE_PRINCIPAL_ID('db_owner');
+
+SELECT DATABASEPROPERTYEX('LambdicSqlTest', 'Collation');
+SELECT DB_NAME(DB_ID ('LambdicSqlTest'));
+SELECT FILE_ID ('LambdicSqlTest');
+SELECT FILE_IDEX ('LambdicSqlTest');
+SELECT FILE_NAME ( FILE_ID ('LambdicSqlTest'));
+SELECT FILEPROPERTY ('LambdicSqlTest', 'IsReadOnly');
+
+
+SELECT FILEGROUP_NAME (FILEGROUP_ID ('LambdicSqlTest'));
+SELECT FILEGROUPPROPERTY('LambdicSqlTest', 'IsReadOnly');
+
+
+SELECT FULLTEXTCATALOGPROPERTY ('Cat_Desc' ,'ItemCount');
+SELECT fulltextserviceproperty('VerifySignature');
+
+
+SELECT INDEX_COL ('dbo', 1,1);
+SELECT INDEXKEY_PROPERTY(OBJECT_ID('Production.Location', 'U'), 1,1,'ColumnId');
+*/
     }
 }
