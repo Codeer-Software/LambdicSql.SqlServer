4be3afad75acfe14b5bf00507207b2abe8d345aa
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-11 11:28:54 +0900

    implementing meta functions,

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Metadata.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Metadata.cs
@@ -1,6 +1,5 @@
 using LambdicSql.ConverterServices;
 using LambdicSql.ConverterServices.SymbolConverters;
-using System;
 
 namespace LambdicSql.SqlServer
 {
@@ -13,19 +12,78 @@ namespace LambdicSql.SqlServer
         /// <param name="obj">db object.</param>
         /// <returns>object id.</returns>
         [MethodFormatConverter(Format = "OBJECT_ID([%0])")]
-        public static int? Object_Id(object obj) { throw new InvalitContextException(nameof(Constraint)); }
-        //TODO type.
-        //やっぱり、stringかなー
-        //ToSqlString的なHelper考えるか？
+        public static int? Object_Id(object obj) => throw new InvalitContextException(nameof(Object_Id));
+
+        /// <summary>
+        /// @@PROCID.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/procid-transact-sql
+        /// </summary>
+        /// <returns>proc id.</returns>
+        [ClauseStyleConverter(Name = "@@PROCID")]
+        public static int AtAtProcId() => throw new InvalitContextException(nameof(AtAtProcId));
+
+        /// <summary>
+        /// APP_NAME.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/app-name-transact-sql
+        /// </summary>
+        /// <returns>app name.</returns>
+        [FuncStyleConverter]
+        public static string App_Name() => throw new InvalitContextException(nameof(App_Name));
+
+        //TODO test.
+        /// <summary>
+        /// APPLOCK_MODE.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/applock-mode-transact-sql
+        /// </summary>
+        /// <param name="database_principal">database_principal.</param>
+        /// <param name="resource_name">resource_name.</param>
+        /// <param name="lock_owner">lock_owner.</param>
+        /// <returns>app lock mode.</returns>
+        [FuncStyleConverter]
+        public static string AppLock_Mode(string database_principal, string resource_name, string lock_owner) => throw new InvalitContextException(nameof(AppLock_Mode));
+
+        //TOTO test.
+        /// <summary>
+        /// APPLOCK_TEST.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/applock-test-transact-sql
+        /// </summary>
+        /// <param name="database_principal">database_principal.</param>
+        /// <param name="resource_name">resource_name.</param>
+        /// <param name="lock_mode">lock_mode.</param>
+        /// <param name="lock_owner">lock_owner.</param>
+        /// <returns>0 or 1.</returns>
+        [FuncStyleConverter]
+        public static short AppLock_Test(string database_principal, string resource_name, string lock_mode, string lock_owner) => throw new InvalitContextException(nameof(AppLock_Test));
+
+        /// <summary>
+        /// ASSEMBLYPROPERTY.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/assemblyproperty-transact-sql
+        /// </summary>
+        /// <param name="assembly_name">assembly_name.</param>
+        /// <param name="property_name">property_name.</param>
+        /// <returns>assembly property.</returns>
+        [FuncStyleConverter]
+        public static object AssemblyProperty(string assembly_name, string property_name) => throw new InvalitContextException(nameof(AssemblyProperty));
+
+        /// <summary>
+        /// COL_LENGTH.
+        /// </summary>
+        /// <param name="table">table.</param>
+        /// <param name="column">column.</param>
+        /// <returns>column length.</returns>
+        [MethodFormatConverter(Format = "COL_LENGTH([%0], [#%1])")]
+        public static short? Col_Length(object table, object column) => throw new InvalitContextException(nameof(Col_Length));
+
+        /// <summary>
+        /// COL_NAME.
+        /// </summary>
+        /// <param name="table_id">table_id.</param>
+        /// <param name="column_id">column_id.</param>
+        /// <returns>column name.</returns>
+        [FuncStyleConverter]
+        public static string Col_Name(int table_id, int column_id) => throw new InvalitContextException(nameof(Col_Name));
 
         /*
-        @@PROCID
-        APP_NAME
-        APPLOCK_MODE
-        APPLOCK_TEST
-        ASSEMBLYPROPERTY
-        COL_LENGTH
-        COL_NAME
         COLUMNPROPERTY
         DATABASE_PRINCIPAL_ID
         DATABASEPROPERTYEX
--- Project/Test40/TestSymbolFuncsMetadata.cs
+++ Project/Test40/TestSymbolFuncsMetadata.cs
@@ -47,5 +47,66 @@ namespace Test
 @"SELECT
 	OBJECT_ID(@p_0)", "tbl_staff");
         }
+
+        [TestMethod]
+        public void Test_AtAtProcId()
+        {
+            var sql = Db<DB>.Sql(db => Select(AtAtProcId()));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	@@PROCID");
+        }
+
+        [TestMethod]
+        public void Test_AppName()
+        {
+            var sql = Db<DB>.Sql(db => Select(App_Name()));
+            
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	APP_NAME()");
+        }
+
+        [TestMethod]
+        public void Test_AssemblyProperty()
+        {
+            var sql = Db<DB>.Sql(db => Select(AssemblyProperty("HelloWorld", "PublicKey")));
+            
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	ASSEMBLYPROPERTY(@p_0, @p_1)", "HelloWorld", "PublicKey");
+        }
+
+        [TestMethod]
+        public void Test_Col_Length()
+        {
+            var sql = Db<DB>.Sql(db => Select(Col_Length(db.tbl_staff, db.tbl_staff.id)));
+            
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	COL_LENGTH(@p_0, @p_1)", "tbl_staff", "id");
+        }
+
+        [TestMethod]
+        public void Test_Col_Name()
+        {
+            var sql = Db<DB>.Sql(db => Select(Col_Name((int)Object_Id(db.tbl_staff), 1)));
+            
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            Assert.AreEqual(sql.Build(_connection.GetType()).Text,
+ @"SELECT
+	COL_NAME(OBJECT_ID(@p_0), @p_1)");
+        }
     }
 }
