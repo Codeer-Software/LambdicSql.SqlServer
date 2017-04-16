2ee67ac85b6d16d70723817963cd0a6c4b4b2bde
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-16 17:42:43 +0900

    implementing collation functions.

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Collation.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Collation.cs
@@ -1,14 +1,25 @@
 using LambdicSql.ConverterServices;
-using LambdicSql.ConverterServices.SymbolConverters;
-using System;
 
 namespace LambdicSql.SqlServer
 {
     public static partial class Symbol
     {
-/*
-COLLATIONPROPERTY
-TERTIARY_WEIGHTS
-*/
+        //----------------@@@---------------
+        /// <summary>
+        /// COLLATIONPROPERTY.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/collation-functions-collationproperty-transact-sql
+        /// </summary>
+        /// <param name="collation_name">collation_name.</param>
+        /// <param name="property">property.</param>
+        /// <returns>Collation property.</returns>
+        public static object CollationProperty(string collation_name , string property ) => throw new InvalitContextException(nameof(CollationProperty));
+
+        /// <summary>
+        /// TERTIARY_WEIGHTS.
+        /// https://docs.microsoft.com/en-us/sql/t-sql/functions/collation-functions-tertiary-weights-transact-sql
+        /// </summary>
+        /// <param name="non_Unicode_character_string_expression">non_Unicode_character_string_expression.</param>
+        /// <returns>Tertiary weights.</returns>
+        public static object Tertiary_Weights(string non_Unicode_character_string_expression) => throw new InvalitContextException(nameof(Tertiary_Weights));
     }
 }
