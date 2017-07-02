c3745df130b758388a7a4d13393b496e84883e60
binnmti <matsui-bin@codeer.co.jp>
2017-07-03 08:58:06 +0900

    FuncsConversion Add Func

--- Project/Test40/TestSymbolFuncsConversion.cs
+++ Project/Test40/TestSymbolFuncsConversion.cs
@@ -6,6 +6,7 @@ using LambdicSql.feat.Dapper;
 using static LambdicSql.SqlServer.Symbol;
 using Test.Helper;
 using LambdicSql.SqlServer;
+using System;
 
 namespace Test
 {
@@ -24,7 +25,7 @@ namespace Test
 
         [TestCleanup]
         public void TestCleanup() => _connection.Dispose();
-        
+
         [TestMethod]
         [Priority(1)]
         public void Test_Cast()
@@ -42,6 +43,139 @@ namespace Test
 @"SELECT
 	CAST(tbl_remuneration.money AS INT) AS id
 FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Convert()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Convert<int>(DataType.Int(), db.tbl_remuneration.money)
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	CONVERT(INT,tbl_remuneration.money) AS id
+FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Parse()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Parse<DateTime>(db.tbl_remuneration.payment_date, DataType.DateTime2())
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	PARSE(tbl_remuneration.payment_date AS DATETIME2) AS id
+FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Parse_Using()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Parse<DateTime>(db.tbl_remuneration.payment_date, DataType.DateTime2(), "en-US")
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	PARSE(tbl_remuneration.payment_date AS DATETIME2 USING @p_0) AS id
+FROM tbl_remuneration", "en-US");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Try_Cast()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Try_Cast<int>(db.tbl_remuneration.money, DataType.Int())
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	TRY_CAST(tbl_remuneration.money AS INT) AS id
+FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Try_Convert()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Try_Convert<int>(DataType.Int(), db.tbl_remuneration.money)
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	TRY_CONVERT(INT,tbl_remuneration.money) AS id
+FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Try_Parse()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Try_Parse<DateTime>(db.tbl_remuneration.payment_date, DataType.DateTime2())
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	TRY_PARSE(tbl_remuneration.payment_date AS DATETIME2) AS id
+FROM tbl_remuneration");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Try_Parse_Using()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new
+                {
+                    id = Try_Parse<DateTime>(db.tbl_remuneration.payment_date, DataType.DateTime2(), "en-US")
+                }).
+                From(db.tbl_remuneration));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	TRY_PARSE(tbl_remuneration.payment_date AS DATETIME2 USING @p_0) AS id
+FROM tbl_remuneration", "en-US");
         }
     }
 }
