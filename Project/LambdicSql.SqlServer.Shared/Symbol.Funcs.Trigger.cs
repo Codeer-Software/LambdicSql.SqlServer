c4986ac5e482ac6cad92884d0e5267bc9eb4d351
binnmti <matsui-bin@codeer.co.jp>
2017-07-04 15:33:34 +0900

    Temp

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Trigger.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Trigger.cs
@@ -6,10 +6,39 @@ namespace LambdicSql.SqlServer
 {
     public static partial class Symbol
     {
-/*
-COLUMNS_UPDATED
-EVENTDATA
-TRIGGER_NESTLEVEL
-UPDATE()*/
+        /*
+        COLUMNS_UPDATED
+        EVENTDATA
+        TRIGGER_NESTLEVEL
+        UPDATE()*/
+
+        /// <summary>
+        /// COLUMNS_UPDATED function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/columns-updated-transact-sql
+        /// </summary>
+        /// <returns>varbinary</returns>
+        [FuncStyleConverter]
+        public static byte[] Columns_Updated() => throw new InvalitContextException(nameof(Columns_Updated));
+
+        /// <summary>
+        /// EVENTDATA function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/eventdata-transact-sql
+        /// </summary>
+        /// <returns>EVENTDATA returns a value of type xml</returns>
+        [FuncStyleConverter]
+        public static string Eventdata() => throw new InvalitContextException(nameof(Eventdata));
+
+        /// <summary>
+        /// TRIGGER_NESTLEVEL function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/trigger-nestlevel-transact-sql
+        /// </summary>
+        /// <returns>varbinary</returns>
+        [FuncStyleConverter]
+        public static int? Trigger_Nestlevel() => throw new InvalitContextException(nameof(Eventdata));
+        public static int? Trigger_Nestlevel(object object_id) => throw new InvalitContextException(nameof(Eventdata));
+        [MethodFormatConverter(Format = "TRIGGER_NESTLEVEL([0], [1], [$2])")]
+        public static int? Trigger_Nestlevel(object object_id, string trigger_type) => throw new InvalitContextException(nameof(Eventdata));
+        [MethodFormatConverter(Format = "TRIGGER_NESTLEVEL([0], [1], [$2], [$3])")]
+        public static int? Trigger_Nestlevel(object object_id, string trigger_type, string trigger_event_category) => throw new InvalitContextException(nameof(Eventdata));
     }
 }
--- Project/Test40/TestSymbolFuncsTrigger.cs
+++ Project/Test40/TestSymbolFuncsTrigger.cs
@@ -24,5 +24,91 @@ namespace Test
 
         [TestCleanup]
         public void TestCleanup() => _connection.Dispose();
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Columns_Updated()
+        {
+            var sql = Db<DB>.Sql(db =>
+               Select(new
+               {
+                   id = db.tbl_staff.id,
+               }).
+               From(db.tbl_staff).
+               Where(Columns_Updated() == null));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	tbl_staff.id AS id
+FROM tbl_staff
+WHERE (COLUMNS_UPDATED()) IS NULL");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Eventdata()
+        {
+            var sql = Db<DB>.Sql(db =>
+               Select(new
+               {
+                   id = db.tbl_staff.id,
+               }).
+               From(db.tbl_staff).
+               Where(Eventdata() == null));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	tbl_staff.id AS id
+FROM tbl_staff
+WHERE (EVENTDATA()) IS NULL");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Trigger_Nestlevel()
+        {
+            var sql = Db<DB>.Sql(db =>
+               Select(new
+               {
+                   id = db.tbl_staff.id,
+               }).
+               From(db.tbl_staff).
+               Where(Trigger_Nestlevel() == 0));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	tbl_staff.id AS id
+FROM tbl_staff
+WHERE (TRIGGER_NESTLEVEL()) = (@p_0)", 0);
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void Test_Trigger_Nestlevel_2()
+        {
+            var sql = Db<DB>.Sql(db =>
+               Select(new
+               {
+                   id = db.tbl_staff.id,
+               }).
+               From(db.tbl_staff).
+               Where(Trigger_Nestlevel(Object_Id("db.tbl_staff.id")) == 0));
+
+            var datas = _connection.Query(sql).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	tbl_staff.id AS id
+FROM tbl_staff
+WHERE (TRIGGER_NESTLEVEL(OBJECT_ID(@p_0))) IS NULL", "XYZ");
+
+        }
+
     }
 }
