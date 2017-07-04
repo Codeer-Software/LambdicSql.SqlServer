17c3a537a8012cd0d754951eff4002691764ce3f
binnmti <matsui-bin@codeer.co.jp>
2017-07-04 16:04:39 +0900

    Refactoring

--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Conversion.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.Conversion.cs
@@ -1,6 +1,5 @@
 using LambdicSql.ConverterServices;
 using LambdicSql.ConverterServices.SymbolConverters;
-using System;
 
 namespace LambdicSql.SqlServer
 {
@@ -15,68 +14,85 @@ namespace LambdicSql.SqlServer
         /// <param name="destinationType">Type of destination.</param>
         /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "CAST(|[0] AS [1])")]
-        public static TDst Cast<TDst>(object target, DataTypeElement destinationType) { throw new InvalitContextException(nameof(Cast)); }
+        public static TDst Cast<TDst>(object target, DataTypeElement destinationType) => throw new InvalitContextException(nameof(Cast));
 
         /// <summary>
         /// CONVERT function.
         /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/cast-and-convert-transact-sql
         /// </summary>
         /// <typeparam name="TDst">Converts an expression of one data type to another data type.</typeparam>
-        /// <param name="target"></param>
-        /// <param name="destinationType">Type of destination.</param>
+        /// <param name="type">Is the target data type. This includes xml, bigint, and sql_variant. Alias data types cannot be used.</param>
+        /// <param name="expression">Is any valid expression.</param>
         /// <returns>Converted data.</returns>
         [FuncStyleConverter]
-        public static TDst Convert<TDst>(DataTypeElement type, object expression) { throw new InvalitContextException(nameof(Convert)); }
+        public static TDst Convert<TDst>(DataTypeElement type, object expression) => throw new InvalitContextException(nameof(Convert));
 
         /// <summary>
         /// PARSE function.
         /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/parse-transact-sql
         /// </summary>
         /// <typeparam name="TDst">Converts an expression of one data type to another data type.</typeparam>
-        /// <param name="value"></param>
-        /// <param name="type">Type of destination.</param>
+        /// <param name="string_value">string_value must be a valid representation of the requested data type, or PARSE raises an error.</param>
+        /// <param name="data_type">Literal value representing the data type requested for the result.</param>
         /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "PARSE([0] AS [1])")]
-        public static TDst Parse<TDst>(object value, DataTypeElement type) { throw new InvalitContextException(nameof(Parse)); }
+        public static TDst Parse<TDst>(object string_value, DataTypeElement data_type) => throw new InvalitContextException(nameof(Parse));
 
+        /// <summary>
+        /// PARSE function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/parse-transact-sql
+        /// </summary>
+        /// <typeparam name="TDst">Converts an expression of one data type to another data type.</typeparam>
+        /// <param name="string_value">string_value must be a valid representation of the requested data type, or PARSE raises an error.</param>
+        /// <param name="data_type">Literal value representing the data type requested for the result.</param>
+        /// <param name="culture">Optional string that identifies the culture in which string_value is formatted.</param>
+        /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "PARSE([0] AS [1] USING [2])")]
-        public static TDst Parse<TDst>(object value, DataTypeElement type, string culture) { throw new InvalitContextException(nameof(Parse)); }
+        public static TDst Parse<TDst>(object string_value, DataTypeElement data_type, string culture) => throw new InvalitContextException(nameof(Parse));
 
         /// <summary>
         /// TRY_CAST function.
         /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/try-cast-transact-sql
         /// </summary>
         /// <typeparam name="TDst">Returns a value cast to the specified data type if the cast succeeds; otherwise, returns null.</typeparam>
-        /// <param name="value"></param>
-        /// <param name="type">Type of destination.</param>
+        /// <param name="expression">The value to be cast. Any valid expression.</param>
+        /// <param name="data_type">The data type into which to cast expression.</param>
         /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "TRY_CAST([0] AS [1])")]
-        public static TDst Try_Cast<TDst>(object value, DataTypeElement type) { throw new InvalitContextException(nameof(Try_Cast)); }
+        public static TDst Try_Cast<TDst>(object expression, DataTypeElement data_type) => throw new InvalitContextException(nameof(Try_Cast));
 
         /// <summary>
         /// TRY_CONVERT function.
         /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/try-convert-transact-sql
         /// </summary>
         /// <typeparam name="TDst">Returns a value cast to the specified data type if the cast succeeds; otherwise, returns null.</typeparam>
-        /// <param name="value"></param>
         /// <param name="type">Type of destination.</param>
+        /// <param name="expression">Is any valid expression.</param>
         /// <returns>Converted data.</returns>
         [FuncStyleConverter]
-        public static TDst Try_Convert<TDst>(DataTypeElement type, object expression) { throw new InvalitContextException(nameof(Try_Convert)); }
+        public static TDst Try_Convert<TDst>(DataTypeElement type, object expression) => throw new InvalitContextException(nameof(Try_Convert));
 
         /// <summary>
         /// TRY_PARSE function.
         /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/try-parse-transact-sql
         /// </summary>
         /// <typeparam name="TDst">Returns a value cast to the specified data type if the cast succeeds; otherwise, returns null.</typeparam>
-        /// <param name="value"></param>
-        /// <param name="type">Type of destination.</param>
+        /// <param name="string_value">nvarchar(4000) value representing the formatted value to parse into the specified data type.</param>
+        /// <param name="data_type">Literal representing the data type requested for the result.</param>
         /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "TRY_PARSE([0] AS [1])")]
-        public static TDst Try_Parse<TDst>(object value, DataTypeElement type) { throw new InvalitContextException(nameof(Try_Parse)); }
+        public static TDst Try_Parse<TDst>(object string_value, DataTypeElement data_type) => throw new InvalitContextException(nameof(Try_Parse));
 
+        /// <summary>
+        /// TRY_PARSE function.
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/try-parse-transact-sql
+        /// </summary>
+        /// <typeparam name="TDst">Returns a value cast to the specified data type if the cast succeeds; otherwise, returns null.</typeparam>
+        /// <param name="string_value">nvarchar(4000) value representing the formatted value to parse into the specified data type.</param>
+        /// <param name="data_type">Literal representing the data type requested for the result.</param>
+        /// <param name="culture">Optional string that identifies the culture in which string_value is formatted.</param>
+        /// <returns>Converted data.</returns>
         [MethodFormatConverter(Format = "TRY_PARSE([0] AS [1] USING [2])")]
-        public static TDst Try_Parse<TDst>(object value, DataTypeElement type, string culture) { throw new InvalitContextException(nameof(Try_Parse)); }
-
+        public static TDst Try_Parse<TDst>(object string_value, DataTypeElement data_type, string culture) => throw new InvalitContextException(nameof(Try_Parse));
     }
 }
--- Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.JSON.cs
+++ Project/LambdicSql.SqlServer.Shared/Symbol.Funcs.JSON.cs
@@ -1,6 +1,5 @@
 using LambdicSql.ConverterServices;
 using LambdicSql.ConverterServices.SymbolConverters;
-using System;
 
 namespace LambdicSql.SqlServer
 {
@@ -13,7 +12,7 @@ namespace LambdicSql.SqlServer
         /// <param name="target">The string to test.</param>
         /// <returns>Returns 1 if the string contains valid JSON; otherwise, returns 0. Returns null if expression is null. Does not return errors.</returns>
         [FuncStyleConverter]
-        public static int? IsJson(object target) { throw new InvalitContextException(nameof(IsJson)); }
+        public static int? IsJson(object target) => throw new InvalitContextException(nameof(IsJson));
 
         /// <summary>
         /// JSON_VALUE function.
@@ -23,7 +22,16 @@ namespace LambdicSql.SqlServer
         /// <param name="path">A JSON path that specifies the property to extract.</param>
         /// <returns>Returns a single text value of type nvarchar(4000). The collation of the returned value is the same as the collation of the input expression.</returns>
         [MethodFormatConverter(Format = "JSON_VALUE([0], [$1])")]
-        public static string Json_Value(object expression, string path) { throw new InvalitContextException(nameof(Json_Value)); }
+        public static string Json_Value(object expression, string path) => throw new InvalitContextException(nameof(Json_Value));
+
+        /// <summary>
+        /// JSON_QUERY
+        /// https://docs.microsoft.com/ja-jp/sql/t-sql/functions/json-query-transact-sql
+        /// </summary>
+        /// <param name="expression">An expression. Typically the name of a variable or a column that contains JSON text.</param>
+        /// <returns>Returns a JSON fragment of type nvarchar(max). The collation of the returned value is the same as the collation of the input expression.</returns>
+        [FuncStyleConverter]
+        public static string Json_Query(object expression) => throw new InvalitContextException(nameof(Json_Query));
 
         /// <summary>
         /// JSON_QUERY
@@ -33,10 +41,7 @@ namespace LambdicSql.SqlServer
         /// <param name="path">A JSON path that specifies the property to extract.</param>
         /// <returns>Returns a JSON fragment of type nvarchar(max). The collation of the returned value is the same as the collation of the input expression.</returns>
         [MethodFormatConverter(Format = "JSON_QUERY([0], [$1])")]
-        public static string Json_Query(object expression, string path) { throw new InvalitContextException(nameof(Json_Query)); }
-
-        [FuncStyleConverter]
-        public static string Json_Query(object expression) { throw new InvalitContextException(nameof(Json_Query)); }
+        public static string Json_Query(object expression, string path) => throw new InvalitContextException(nameof(Json_Query));
 
         /// <summary>
         /// JSON_MODIFY
@@ -47,6 +52,6 @@ namespace LambdicSql.SqlServer
         /// <param name="newValue">The new value for the property specified by path.</param>
         /// <returns>Returns the updated value of expression as properly formatted JSON text.</returns>
         [MethodFormatConverter(Format = "JSON_MODIFY([0], [$1], [2])")]
-        public static string Json_Modify(object expression, string path, string newValue) { throw new InvalitContextException(nameof(Json_Value)); }
+        public static string Json_Modify(object expression, string path, string newValue) => throw new InvalitContextException(nameof(Json_Value));
     }
 }
