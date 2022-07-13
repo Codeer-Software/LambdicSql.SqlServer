33f2125d8d6abeeca825980ad0a862dac74f2c8d
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2022-07-13 23:55:04 +0900

    version up

--- Project/LambdicSql.SqlServer.NetFramework.3.5/Properties/AssemblyInfo.cs
+++ Project/LambdicSql.SqlServer.NetFramework.3.5/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("335592c4-241d-4e16-94e0-b1a23fc279b9")]
 
-[assembly: AssemblyVersion("0.29.0.0")]
-[assembly: AssemblyFileVersion("0.29.0.0")]
+[assembly: AssemblyVersion("0.30.0.0")]
+[assembly: AssemblyFileVersion("0.30.0.0")]
 
--- Project/LambdicSql.SqlServer.PCL/Properties/AssemblyInfo.cs
+++ Project/LambdicSql.SqlServer.PCL/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("ja")]
 
-[assembly: AssemblyVersion("0.29.0.0")]
-[assembly: AssemblyFileVersion("0.29.0.0")]
+[assembly: AssemblyVersion("0.30.0.0")]
+[assembly: AssemblyFileVersion("0.30.0.0")]
 
